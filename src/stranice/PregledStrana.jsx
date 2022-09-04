9fc80c6ad7ddf2fcb2ed19d340516c01a7a3db2c
AnjaDrobnjak1 <drobnjakanaaa@gmail.com>
2022-09-04 23:58:01 +0200

    Ispis podataka iz data za proveru

--- src/stranice/PregledStrana.jsx
+++ src/stranice/PregledStrana.jsx
@@ -8,6 +8,13 @@ function PregledStrana() {
     return (
         <div>
             <h1 id='chck'>Molimo Vas proverite Vaše podatke</h1>
+
+            <div className='podaci'>
+                <h3>Ime -  {data.ime}</h3>
+                <h3>Prezime - {data.prezime}</h3>
+                <h3>Email - {data.email}</h3>
+                <h3>Broj telefona - {data.broj_telefona}</h3>
+            </div>
         </div>
     )
 }
